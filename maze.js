980026fded62bc6d8c2c4dd1b476ce6190ae3bf5
Daud Dhiya <daud.dhiya31@gmail.com>
2023-05-05 14:18:55 +0700

    fix: flow program

--- maze.js
+++ maze.js
@@ -1,4 +1,5 @@
 const canvas = document.querySelector("canvas");
+const cost = document.getElementById("cost");
 const canvasContext = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -9,16 +10,10 @@ let path = [];
 let solutions = [];
 let start, end;
 
-const tc = 1;
-const boundaries = [];
-const pellets = [];
-const map = [
-  ["-", "-", "-", "-", "-", "-"],
-  ["-", ".", ".", ".", ".", "-"],
-  ["-", ".", "-", "-", ".", "-"],
-  ["-", ".", ".", ".", ".", "-"],
-  ["-", "-", "-", "-", "-", "-"],
-];
+let tc = 1;
+let boundaries = [];
+let pellets = [];
+let map = [];
 
 const keys = {
   w: {
@@ -185,7 +180,7 @@ class Maze {
         tmp.push(now.x, now.y);
         solutions.push(tmp);
       });
-      console.log(`path length: ${path.length}`);
+      cost.innerHTML = path.length;
 
       solutions.forEach((solution) => {
         console.log(solution);
@@ -210,90 +205,116 @@ class Maze {
 
 // STARTING PROGRAM
 
-const maze = new Maze();
+function mulai() {
+  path = [];
+  solutions = [];
 
-for (let i = 1; i <= tc; i++) {
-  start = [1, 1];
-  end = [3, 2];
-  console.log(`CASE ${i}: `);
-  maze.shortestPath(map, start, end);
-}
+  boundaries = [];
+  pellets = [];
+  map = [];
 
-map.forEach((row, i) => {
-  row.forEach((symbol, j) => {
-    switch (symbol) {
-      case "-":
-        boundaries.push(
-          new Boundary({
-            position: { x: Boundary.width * j, y: Boundary.height * i },
-          })
-        );
-        break;
+  function generateMaze() {
+    const lst = [".", "#", ".", ".", "."];
+    const row = Math.floor(Math.random() * 31) + 10;
+    const col = Math.floor(Math.random() * 31) + 10;
+
+    for (let i = 0; i < row; i++) {
+      const tmp = [];
+      for (let j = 0; j < col; j++) {
+        tmp.push(lst[Math.floor(Math.random() * lst.length)]);
+      }
+      map.push(tmp);
     }
-  });
-});
-
-const player = new Player({
-  position: {
-    x: Boundary.width + Boundary.width / 2,
-    y: Boundary.height + Boundary.height / 2,
-  },
-  velocity: { x: 0, y: 0 },
-});
-
-for (let i = solutions.length - 1; i >= 0; i--) {
-  const solution = solutions[i];
-  pellets.push(
-    new Pellet({
-      position: {
-        x: Boundary.width * solution[1] + Boundary.width / 2,
-        y: Boundary.height * solution[0] + Boundary.height / 2,
-      },
-    })
-  );
-}
-
-function animate() {
-  requestAnimationFrame(animate);
-  canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-
-  boundaries.forEach((boundary) => {
-    boundary.draw();
-  });
-
-  for (let i = 0; i < pellets.length; i++) {
-    const pellet = pellets[i];
-    // console.log(pellet.position.x);
-    pellet.draw();
   }
-  player.update();
 
-  // buat harus pressed
-  player.velocity.y = 0;
-  player.velocity.x = 0;
+  generateMaze();
+
+  const maze = new Maze();
+
+  for (let i = 1; i <= tc; i++) {
+    start = [1, 1];
+    end = [10, 9];
+    console.log(`CASE ${i}: `);
+    maze.shortestPath(map, start, end);
+  }
+
+  map.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+      switch (symbol) {
+        case "#":
+          boundaries.push(
+            new Boundary({
+              position: { x: Boundary.width * j, y: Boundary.height * i },
+            })
+          );
+          break;
+      }
+    });
+  });
+
+  const player = new Player({
+    position: {
+      x: Boundary.width + Boundary.width / 2,
+      y: Boundary.height + Boundary.height / 2,
+    },
+    velocity: { x: 0, y: 0 },
+  });
+
+  for (let i = solutions.length - 1; i >= 0; i--) {
+    const solution = solutions[i];
+    pellets.push(
+      new Pellet({
+        position: {
+          x: Boundary.width * solution[1] + Boundary.width / 2,
+          y: Boundary.height * solution[0] + Boundary.height / 2,
+        },
+      })
+    );
+  }
+
+  function animate() {
+    requestAnimationFrame(animate);
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+
+    boundaries.forEach((boundary) => {
+      boundary.draw();
+    });
+
+    for (let i = 0; i < pellets.length; i++) {
+      const pellet = pellets[i];
+      // console.log(pellet.position.x);
+      pellet.draw();
+    }
+    player.update();
+
+    // buat harus pressed
+    player.velocity.y = 0;
+    player.velocity.x = 0;
+  }
+
+  for (let i = 0; i < path.length - 1; i++) {
+    const xNow = path[i + 1].x - path[i].x;
+    const yNow = path[i + 1].y - path[i].y;
+
+    setTimeout(() => {
+      if (xNow < 0) player.velocity.y = -40;
+      else if (xNow > 0) player.velocity.y = 40;
+      else if (yNow < 0) player.velocity.x = -40;
+      else if (yNow > 0) player.velocity.x = 40;
+
+      player.update();
+      // buat harus pressed
+      player.velocity.y = 0;
+      player.velocity.x = 0;
+    }, 2000);
+  }
+
+  addEventListener("keydown", ({ key }) => {
+    if (key === "w") player.velocity.y = -40;
+    else if (key === "a") player.velocity.x = -40;
+    else if (key === "s") player.velocity.y = 40;
+    else if (key === "d") player.velocity.x = 40;
+  });
+
+  animate();
 }
-
-for (let i = 0; i < path.length - 1; i++) {
-  const xNow = path[i + 1].x - path[i].x;
-  const yNow = path[i + 1].y - path[i].y;
-
-  if (xNow < 0) player.velocity.y = -40;
-  else if (xNow > 0) player.velocity.y = 40;
-  else if (yNow < 0) player.velocity.x = -40;
-  else if (yNow > 0) player.velocity.x = 40;
-
-  player.update();
-
-  // buat harus pressed
-  player.velocity.y = 0;
-  player.velocity.x = 0;
-}
-
-addEventListener("keydown", ({ key }) => {
-  if (key === "w") player.velocity.y = -40;
-  else if (key === "a") player.velocity.x = -40;
-  else if (key === "s") player.velocity.y = 40;
-  else if (key === "d") player.velocity.x = 40;
-});
-
-animate();
