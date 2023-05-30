68c358e678ca4ce0d2374d01757b70f8af192e7e
Daud Dhiya <daud.dhiya31@gmail.com>
2023-05-30 11:49:45 +0700

    refactor: split up image variables

---
+++ interactive_maze/images.py
@@ -0,0 +1,18 @@
+from pygame import image as img
+
+# background
+startup_img = img.load("assets/img/startup_img.png")
+guidepage_img = img.load("assets/img/guide_page.png")
+main_img = img.load("assets/img/main_img.png")
+modepage_img = img.load("assets/img/mode_page.png")
+# main menu
+start_img = img.load("assets/img/start_img.png")
+guide_img = img.load("assets/img/guide_img.png")
+exit_img = img.load("assets/img/exit_img.png")
+back_img = img.load("assets/img/back_img.png")
+# mode menu
+easy_mode = img.load("assets/img/easy_mode.png")
+medium_mode = img.load("assets/img/medium_mode.png")
+hard_mode = img.load("assets/img/hard_mode.png")
+random_mode = img.load("assets/img/random_mode.png")
+custom_mode = img.load("assets/img/custom_mode.png")
